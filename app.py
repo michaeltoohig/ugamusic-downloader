692ade799d8dbf1c996de3fa0010a909c5e831b3
Michael Toohig <michaeltoohig@gmail.com>
2019-07-20 22:51:20 +1100

    Add tasks. WIP needs restructuring

--- app.py
+++ app.py
@@ -2,36 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from tinydb import TinyDB, Query
-from huey import SqliteHuey
+from tinydb.operations import set
+from huey import SqliteHuey, crontab
 
 db = TinyDB('stores/ugamusic.db')
-huey = SqliteHuey('tasks.db')
-
-if False:
-    page = requests.get('https://ugamusic.biz/hot100')
-    page = page.text
-    with open('top100.html', 'w') as f:
-        f.write(page)
-else:
-    f = open('top100.html', 'r')
-    page = f.read()
-    f.close()
-
-
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page, 'html.parser')
-
-# Pull all text from the BodyText div
-top_100_table = soup.find('table', {'id': 'table-chart'})
-
-# Pull text from all instances of <a> tag within BodyText div
-chart_rows = top_100_table.find('tbody').find_all('tr')
+huey = SqliteHuey('./tasks.db')
 
 # Create query object for our db
 Songs = Query()
 
+
+#@huey.task(retries=3, retry_delay=300)
+def fetch_download_link(page):
+    '''Find the download link on the song page.'''
+    response = requests.get(page)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    link = soup.find('a', class_='song-download', href=True).get('href')
+    db.update({'download_link': link, 'download_at': False}, Songs.page == page)
+
+
+@huey.task(retries=3, retry_delay=300)
+def fetch_song(link):
+    '''Download the song.'''
+    song = db.search(Songs.download_link == link)
+    if not song:
+        pass
+    song = song[0]
+    print('fetching song')
+    response = requests.get(link)
+    print(response)
+    with open(f'downloads/{song.get("artist")} - {song.get("title")}.mp3', 'wb') as f:
+        f.write(response.content)
+    db.update({'download_at': datetime.utcnow().timestamp()}, Songs.download_link == link)
+
+
+# Get page
+#response = requests.get('https://ugamusic.biz/hot100')
+#with open('pages/ugamusic-hot100.html', 'w') as f:
+#    f.write(response.text)
+
+with open('pages/ugamusic-hot100.html', 'r') as f:
+    page = f.read()
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(page, 'html.parser')
+
+# Pull the hot100 table
+top_100_table = soup.find('table', {'id': 'table-chart'})
+
+# Pull all the rows from the table body
+chart_rows = top_100_table.find('tbody').find_all('tr')
+
 # Pull details from each row
-for chart_row in chart_rows:
+for chart_row in chart_rows[:3]:
     title = chart_row.find(class_='titlemeta')
     artist = chart_row.find(class_='namemeta')
     link = chart_row.find(class_='play-song', href=True)
@@ -43,6 +66,19 @@ for chart_row in chart_rows:
             'artist': artist.text, 
             'title': title.text, 
             'page': link['href'], 
-            'downlaod_link': None,
-            'download_at': None,
+            'download_link': False,
         })
+
+
+
+# Trigger tasks to get the download links
+for item in db.search(Songs.download_link == False):
+    #print(item)
+    fetch_download_link(item.get('page'))
+
+# Trigger tasks to get the songs
+for item in db.search((Songs.download_link.exists()) & (Songs.download_at == False)):
+    print(item)
+    fetch_song(item.get('download_link'))
+
+print('done')
---
+++ run_huey.py
@@ -0,0 +1,2 @@
+from app import huey, fetch_download_link, fetch_song
+
